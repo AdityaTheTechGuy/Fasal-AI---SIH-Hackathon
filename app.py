63da41f3b844093703945ea7ce30c477217ea001
AdityaTheTechGuy <Adityaguptaofficial1@gmail.com>
2025-09-15 20:49:00 +0530

    Hosting

--- app.py
+++ app.py
@@ -228,8 +228,5 @@ def api_predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
-
-
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
